e5837e6435ad96419ff3de7c93d13be3bdd07a93
LuckyIYI <lacoz@icloud.com>
2026-09-10 21:31:40 -0700

    Preserve detailed convex assets with scene-sized clipping and exact picking

--- Package.swift
+++ Package.swift
@@ -39,7 +39,8 @@ let package = Package(
         ),
         .testTarget(
             name: "PhysicsAVBDTests",
-            dependencies: ["SimCore", "PhysicsAVBD", "GPUSimDemos"]
+            dependencies: ["SimCore", "PhysicsAVBD", "GPUSimDemos"],
+            resources: [.copy("Fixtures/ConvexCapacity")]
         ),
         .testTarget(
             name: "GPUSimTests",
